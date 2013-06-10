bc3cf62397dd5c1d03ba7b3d91081b92b26d0c29
Eric Dunsworth <eric.dunsworth@hrsdc-rhdcc.gc.ca>
2013-06-10 17:02:34 -0400

    Experimental/WIP commit.

--- src/js/workers/footnotes.js
+++ src/js/workers/footnotes.js
@@ -18,11 +18,14 @@
 			var _ctn = _pe.main.not('.wet-boew-footnotes'), //reference to the content area (which needs to be scanned for footnote references)
 				footnote_dd = elm.find('dd').attr('tabindex', '-1');
 
-			// Apply aria-labelledby and set initial event handlers for return to referrer links
+			// Apply aria-labelledby, escape special characters in attributes, and set initial event handlers for return to referrer links
 			footnote_dd.each(function () {
 				var $this = $(this),
-					dtid = this.id + '-dt';
+					ddid = _pe.string.jqescape(this.id),
+					dtid = ddid + '-dt',
+					ahref = '#' + _pe.string.jqescape($this.find('p.footnote-return a').attr('href').substring(1));
 				$this.attr('aria-labelledby', dtid).prev().attr('id', dtid);
+				$this.find('p.footnote-return a').attr('href', ahref);
 			});
 
 			//remove "first/premier/etc"-style text from certain footnote return links (via the child spans that hold those bits of text)
@@ -30,11 +33,17 @@
 				var $this = $(this);
 				$this.find('span span').remove();
 				$this.off('click vclick').on('click vclick', function () {
-					var referrer = _ctn.find($(this).attr('href')).find('a');
+					
+					alert(_pe.string.jqescape($(this).attr('href')).substring(1));
+					
+					var _referrer = _ctn.find($(this).attr('href')).find('a');
+					
+					//alert(_referrer.attr('href').substr(1));
+					
 					if (_pe.mobile) {
-						$.mobile.silentScroll(_pe.focus(referrer).offset().top);
+						$.mobile.silentScroll(_pe.focus(_referrer).offset().top);
 					} else {
-						_pe.focus(referrer);
+						_pe.string.jqescape($(this).attr('href')).substring(1).find('a').focus();
 					}
 					return false;
 				});
@@ -44,13 +53,17 @@
 			_ctn.find('sup a.footnote-link').on('click vclick', function () {
 				//captures certain information about the clicked link
 				var _refLinkDest = elm.find($(this).attr('href'));
+				
+				//alert(_pe.string.jqescape($(this).parent().attr('id')));
+				//this.parentNode.attr('id', _pe.string.jqescape(this.parentNode.id));
+				//alert(this.parentNode.id);
 
 				_refLinkDest.find('p.footnote-return a').attr('href', '#' + this.parentNode.id).off('click vclick').on('click vclick', function () {
-					var referrer = _ctn.find($(this).attr('href')).find('a');
+					var _referrer = _ctn.find($(this).attr('href')).find('a');
 					if (_pe.mobile === true) {
-						$.mobile.silentScroll(_pe.focus(referrer).offset().top);
+						$.mobile.silentScroll(_pe.focus(_referrer).offset().top);
 					} else {
-						_pe.focus(referrer);
+						_pe.focus(_referrer);
 					}
 					return false;
 				});
