ff9f5e066309673068dc23832e8f22e5037d949a
Eric Dunsworth <1907279+EricDunsworth@users.noreply.github.com>
2022-05-16 13:17:17 -0400

    Updates

--- wet-boew/js/ie8-wet-boew2.js
+++ wet-boew/js/ie8-wet-boew2.js
@@ -8888,7 +8888,7 @@ $document.on( initializedEvent, selector, function( event ) {
 
 	//console.log( "media is YouTube" );
 
-	if ( event.namespace === componentName && media ) ) {
+	if ( event.namespace === componentName && media ) {
 		console.log( "inside if" );
 
 		var captions = $media.children( "track[kind='captions']" ).attr( "src" ) || undef,
