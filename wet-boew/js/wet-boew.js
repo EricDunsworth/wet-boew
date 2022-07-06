df7756ec9008a79dd36c3549e1dd3356094f1ea5
Eric Dunsworth <1907279+EricDunsworth@users.noreply.github.com>
2022-07-06 16:05:07 -0400

    Updates

--- wet-boew/js/wet-boew.js
+++ wet-boew/js/wet-boew.js
@@ -11601,18 +11601,18 @@ $document.on( initializedEvent, selector, function( event ) {
 				$this.trigger( youtubeEvent, data );
 			}
 
-			if (window.youTube.ready) {
-				//alert("0 YT is ready!!!");
-			}
-			else {
-				//alert("0 YT failed!!!");
-			}
-
 			// finally lets load safely
 			return Modernizr.load( {
 				load: "https://www.youtube.com/iframe_api",
 				complete: function() {
 					alert("done!");
+
+					if (window.youTube.ready) {
+						alert("0 YT is ready!!!");
+					}
+					else {
+						alert("0 YT failed!!!");
+					}
 				}
 
 			} );
